4febe96a41dc9fccb997f9af7b3aa3c1102f236f
William Marsman <william.marsman@gmail.com>
2020-04-18 15:43:29 -0300

    WIP: Acceptance criteria for 'Selenium Install'

--- src/app/settings/selenium/install/install.component.spec.ts
+++ src/app/settings/selenium/install/install.component.spec.ts
@@ -28,26 +28,33 @@ describe('InstallComponent', () => {
   describe('Install Component Integration Test', () => {
     it('should be able to ' +
       'display selenium is not installed, ' +
+      'display firefox is not installed, ' +
       'display chrome is not installed, ' +
-      'when the user, ' +
-      'selects a version of selenium, ' +
-      'a version of the chrome driver, ' +
-      '& clicks "install selenium", ' +
-      'and then, ' +
-      'install progress should be updated until installed, ' +
-      'and then once installation is complete, ' +
+      'display ie is not installed & ' +
+      'display edge is not installed; ' +
+      'then when the user ' +
+      'selects a version of selenium & ' +
+      'clicks "install"; ' +
+      'then installation should begin & ' +
+      'install progress should be updated until installed; ' +
+      'then when installation is complete ' +
       'display a notification indicating installation is complete, ' +
       'display the install path to selenium, ' +
       'display the installed version of selenium, ' +
-      '& display the installed version of the chrome driver', () => {
+      'display the installed version of the firefox driver, ' +
+      'display the installed version of the chrome driver, ' +
+      'display the installed version of the ie driver & ' +
+      'display the installed version of the edge driver', () => {
         // Display
         const seleniumInstallPath = htmlElement.querySelector('#selenium-install-path');
         const seleniumVersionInstalled = htmlElement.querySelector('#selenium-version-installed');
+        const firefoxVersionInstalled = htmlElement.querySelector('#firefox-version-installed');
         const chromeVersionInstalled = htmlElement.querySelector('#chrome-version-installed');
+        const edgeVersionInstalled = htmlElement.querySelector('#edge-version-installed');
+        const ieVersionInstalled = htmlElement.querySelector('#ie-version-installed');
 
         // Interactable
         const seleniumVersionSelector = htmlElement.querySelector("#selenium-version-selector");
-        const chromeVersionSelector = htmlElement.querySelector("#chrome-version-selector");
         const installSeleniumSubmit = htmlElement.querySelector('#install-selenum-submit');
         const installSeleniumProgress = htmlElement.querySelector('#install-selenium-progress');
         const installSeleniumCompleteNotification = htmlElement.querySelector('#install-selenium-complete-notification');
@@ -55,11 +62,13 @@ describe('InstallComponent', () => {
 
         // Display elements should be present.
         expect(seleniumVersionSelector).toBeDefined();
-        expect(chromeVersionSelector).toBeDefined();
         expect(installSeleniumSubmit).toBeDefined();
         expect(seleniumInstallPath).toBeDefined();
         expect(seleniumVersionInstalled).toBeDefined();
         expect(chromeVersionInstalled).toBeDefined();
+        expect(firefoxVersionInstalled).toBeDefined();
+        expect(edgeVersionInstalled).toBeDefined();
+        expect(ieVersionInstalled).toBeDefined();
 
         // Progress and Notification elements not yet present.
         expect(installSeleniumProgress).toBeUndefined();
